a72ab9aba9a8f754b8f58ab3e3264909f90b1da0
Ashfakul Islam <r4pido3@gmail.com>
2024-06-03 20:42:06 +0600

    mongoose error handle function created

--- src/app/middleware/globalErrHandler.ts
+++ src/app/middleware/globalErrHandler.ts
@@ -5,12 +5,14 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { ZodError, ZodIssue } from 'zod';
 import { TErrorSources } from '../interface/error';
 import config from '../config';
+import handleZodError from '../errors/handlezoderror';
+import handelValidationError from '../errors/handleValidationError';
+import handelCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateId';
 
 const globalErrHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode: number = err.statusCode || 500;
   let message: any = err.message || 'Something went wrong';
-
- 
 
   let errorSources: TErrorSources = [
     {
@@ -19,39 +21,35 @@ const globalErrHandler: ErrorRequestHandler = (err, req, res, next) => {
     },
   ];
 
-const handleZodError = (err : ZodError) => {
-
-  const errorSource : TErrorSources = err.issues.map((issue : ZodIssue) => {
-    return{
-      path: issue?.path[issue.path.length-1],
-      message: issue?.message,
-    }
-  })
-
-  const  statusCode = 400;
-  return{
-    statusCode,
-    message: "Validation error",
-    errorSources: errorSource
-  }
-}
-
-
-
-  if (err instanceof ZodError){
-   const simplifiedError =  handleZodError(err)
-   statusCode = simplifiedError.statusCode;
-   message = simplifiedError.message;
-   errorSources = simplifiedError.errorSources
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handelValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handelCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if ( err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
 
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      errorSources,
-      stack: config.node_env === 'development' ? err?.stack : null,
-    });
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.node_env === 'development' ? err?.stack : null,
+  });
 };
 
 export default globalErrHandler;
